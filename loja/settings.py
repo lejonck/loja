14502c0d59c235ca30303569e8cb24526f7a5206
Letícia Raddatz Jönck <leticiarjonck@gmail.com>
2024-07-26 23:52:57 -0300

    db

--- loja/settings.py
+++ loja/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'loja_django',
         'USER': 'adm-loja',
-        'PASSWORD': 'senhaloja',
+        'PASSWORD': os.getenv('GRACE_DB_PASSWORD', 'default_password_if_not_set'),
         'HOST': 'localhost',
         'PORT': '3306',
     }
@@ -119,7 +120,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-import os
 
 STATIC_URL = 'static/'
 
@@ -131,7 +131,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
